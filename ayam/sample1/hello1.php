1e3ece2cca3368a3e96487d7330df67b60d687e0
DarshiniG <guhadarshini10@gmail.com>
2018-06-18 10:19:33 +0800

    first commit

---
+++ ayam/sample1/hello1.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+  <meta http-equiv="content-type" content="text/html;charset=utf-8"/>
+  <title>First PHP Script</title>
+ </head>
+ 
+ <body>
+ <?php
+ 
+ phpinfo();
+ ?>
+ </body>
+  
+</html>
